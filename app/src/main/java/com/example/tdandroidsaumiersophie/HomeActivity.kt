5f5f9764b11b3a8c94ce7ca8441bab9700cf1e1f
sophi <sophie.saumier@isen.yncrea.fr>
2021-01-25 13:51:49 +0100

    premier commit on essaye de faire une deuxième page

--- app/src/main/java/com/example/tdandroidsaumiersophie/HomeActivity.kt
+++ app/src/main/java/com/example/tdandroidsaumiersophie/HomeActivity.kt
@@ -2,10 +2,19 @@ package com.example.tdandroidsaumiersophie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tdandroidsaumiersophie.databinding.ActivityHomeBinding
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button4.setOnClickListener{
+            Toast.makeText(applicationContext, "oui", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
